71e2f31a24ac66fbdfd1c345df93fd685f012667
Wilson Calderazzi <71198481+calderazzi@users.noreply.github.com>
2022-10-30 01:38:06 -0300

    cambio de vista

---
+++ espera.js
@@ -0,0 +1,15 @@
+setTimeout(function(){
+  document.getElementById("video").style.display = "inline";
+},2500);
+setTimeout(function(){
+  document.getElementById("img1").style.display = "inline";
+},6000);
+setTimeout(function(){
+  document.getElementById("img2").style.display = "inline";
+},8000);
+setTimeout(function(){
+  document.getElementById("img3").style.display = "inline";
+},10000);
+setTimeout(function(){
+  document.getElementById("img4").style.display = "inline";
+},12000);
